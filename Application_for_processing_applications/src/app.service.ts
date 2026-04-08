a9fceb9ec9a3e2dcc655a4f89015c05d55a1e9f9
Dmitrii Ozornin <ozornindimitry@uandex.ru>
2026-04-08 20:32:21 +0300

    поправил репозиторий

--- Application_for_processing_applications/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { LoginUserDTO } from './loginUser.dto';
-
-@Injectable()
-export class AppService {
-  login(user: LoginUserDTO): string {
-    return `${user.login}  ${user.password}`;
-  }
-}
